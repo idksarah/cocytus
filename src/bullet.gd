7741e6b9c39a9988f062f2746eadc0238b053470
idksarah <sarah.p.ngai@gmail.com>
2025-02-16 10:32:51 -0800

    art

--- src/bullet.gd
+++ src/bullet.gd
@@ -10,34 +10,40 @@ var multiplier = 500
 
 var reg_timer = 1
 var still_timer = 3
+var pre_still_time = 0.05
 var still_shoot = false
+var stopped = false
 
 @onready var player : CharacterBody2D = $"../Player"
 @onready var timer = $Timer
+@onready var stop_timer = $StopTimer
+@onready var collision = $CollisionShape2D
 
 func _ready() -> void:
 	if still_shoot:
 		timer.wait_time = still_timer
+		stop_timer.wait_time = pre_still_time
+		stop_timer.start()
 	else:
 		timer.wait_time = reg_timer
 	global_position = Vector2(pos.x + x_pos_offset, pos.y + y_pos_offset)
-	print(timer.wait_time)
-	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
 	
-	
 func _process(delta: float) -> void:
-	#print(still_shoot)
 	pass
 	
 func _on_timer_timeout():
 	queue_free()
 
+func _on_stop_timer_timeout():
+	stopped = true
+
 func _physics_process(delta: float):
 	velocity = Vector2(x_vector * multiplier, y_vector * multiplier)
-	if still_shoot:
+	if stopped:
 		velocity = Vector2(0,0)
 	move_and_slide()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_collision_shape_2d_child_entered_tree(node: Node) -> void: # bullet should disappear when hit stuff but idc
+	print('fuck ass')
 	queue_free()
--- src/player.gd
+++ src/player.gd
@@ -9,7 +9,6 @@ var char_pos : Vector2
 var cur_speed = 0.0
 const reg_speed = 300.0
 const giga_speed = 400.0
-const jump_velocity = -800.0
 const coyote_time = 0.2
 var curr_coyote_time = 0
 var move_duration = 0.1
@@ -29,6 +28,10 @@ var max_x_direction = 4
 var max_y_direction = 3
 var bullet_x_offset = 10
 var bullet_y_offset = 10
+
+
+var player_x_direction := Input.get_axis("better_left", "better_right")
+var player_y_direction := Input.get_axis("better_down", "better_up")
 
 enum state {left_shoot, right_shoot, down_shoot, up_shoot, left_down_shoot, left_up_shoot, right_down_shoot, right_up_shoot}
 var current_state : state
@@ -90,50 +93,49 @@ func track_mouse(delta):
 	elif (Input.is_action_just_pressed("still_shoot")):
 		still_shoot = true
 		player_shoot(delta)
+		
 	# continue moving for move_duration even after player releases key
-	elif move_timer > 0:
+	elif move_timer > 0: # should change this to an actual timer u fuckass
 		move_timer-= delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, reg_speed)
 
 func player_shoot(delta):
-	print(still_shoot)
 	if(is_on_floor()):
 		cur_bullets_shot = 0
-	var x_direction := Input.get_axis("better_left", "better_right")
-	var y_direction := Input.get_axis("better_down", "better_up")
+		
 	
 	if(is_on_floor() || cur_bullets_shot < max_bullets_shot):
+		player_x_direction = -(mouse_pos.x - char_pos.x) * x_direction_multiplier
+		player_y_direction = -(mouse_pos.y - char_pos.y) * y_direction_multiplier
 		
-		x_direction = -(mouse_pos.x - char_pos.x) * x_direction_multiplier
-		y_direction = -(mouse_pos.y - char_pos.y) * y_direction_multiplier
+		player_x_direction = clamp(player_x_direction, -max_x_direction, max_x_direction)
+		player_y_direction = clamp(player_y_direction, -max_y_direction, max_y_direction)
 		
-		x_direction = clamp(x_direction, -max_x_direction, max_x_direction)
-		y_direction = clamp(y_direction, -max_y_direction, max_y_direction)
-		
-		velocity.x = x_direction * reg_speed
-		velocity.y = y_direction * reg_speed
+		velocity.x = player_x_direction * reg_speed
+		velocity.y = player_y_direction * reg_speed
 		move_timer = move_duration
 		
 		if not is_on_floor():
 			cur_bullets_shot+=1
 		
+		# bullet
 		await get_tree().create_timer(.2).timeout
 		
 		var bullet_instance = bullet.instantiate() as Node2D
 		bullet_instance.pos = animator.global_position
-		if x_direction > 0:
+		
+		if player_x_direction > 0:
 			bullet_instance.x_pos_offset *= -1
-		if y_direction > 0:
+			
+		if player_y_direction > 0:
 			bullet_instance.y_pos_offset *= -1
-		bullet_instance.x_vector = -x_direction
-		bullet_instance.y_vector = -y_direction
+		bullet_instance.x_vector = -player_x_direction
+		bullet_instance.y_vector = -player_y_direction
+		
 		if still_shoot:
 			bullet_instance.still_shoot = true
-			get_parent().add_child(bullet_instance)
-			await get_tree().create_timer(.2).timeout
-			bullet_instance.velocity = Vector2(0,0)
-		else:
-			get_parent().add_child(bullet_instance)
+			
+		get_parent().add_child(bullet_instance)
 		
 		
