bdf5456765708698f40d9a52a55b41d856be9744
idksarah <sarah.p.ngai@gmail.com>
2025-03-21 00:47:03 -0700

    rm src/ ; mv new_src/ src/

--- new_src/camera.gd
+++
@@ -1,4 +0,0 @@
-extends Camera2D
-
-func zoom(p_zoom):
-	set_zoom(p_zoom)
--- new_src/checkpoint_manager.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-var player
-
-func _ready() -> void:
-	player = get_parent().get_node("Player")
-	if Singleton.last_checkpoint == null:
-		Singleton.last_checkpoint = player.global_position
