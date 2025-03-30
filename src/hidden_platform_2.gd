5e10b6a7c245b46e658ee98beb63d911976e8535
idksarah <sarah.p.ngai@gmail.com>
2025-03-29 18:28:32 -0700

    tiles lol

---
+++ src/hidden_platform_2.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var platform_1_area = $Area2D
+@onready var platform_2_area = $Area2D2
+@onready var platform_3_area = $Area2D3
+@onready var platform_4_area = $Area2D4
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	platform_2_area.visible = false
+	platform_3_area.visible = false
+	platform_4_area.visible = false
+
+func _on_area_2d_area_entered(_area: Area2D) -> void:
+	platform_2_area.visible = true
+
+func _on_area_2d_2_area_entered(area: Area2D) -> void:
+	platform_3_area.visible = true
+
+func _on_area_2d_3_area_entered(area: Area2D) -> void:
+	platform_4_area.visible = true
